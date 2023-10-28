1371657bfa28b73a10fe47d5abad9bfe948a3f80
Tim Duckett <tim@duckett.de>
2023-10-28 21:57:57 +0200

    cleanup comments

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
+import { gitAdd, gitCommit, gitPush } from './gitHelper';
 import { saveToLocalDatabase, fetchBearData } from './dbHelpers';
-import { gitAdd, gitCommit, gitPush, performGitOperations } from './gitHelper';
 import { connectToBearDb, connectToLocalDb, getNoteContent } from './dbHelpers';
 import { createBlogContent, createBlogPostFiles, gitTimestamp } from './postHelpers';
 
@@ -16,27 +16,22 @@ async function main() {
   const taggedNotes = await fetchBearData(bearDb);
 
   if (taggedNotes) {
-    console.log(`taggedNotes.length = ${taggedNotes.length}`)
     await saveToLocalDatabase(localDb, taggedNotes);
   }
   
   const notes = await getNoteContent(localDb);
-  console.log(`notes.length = ${notes.length}`)
-
+  
   const posts = createBlogContent(notes);
-  console.log(`posts.length = ${posts.length}`)
-
+  
   await createBlogPostFiles(posts);
 
   const directory = process.env.HUGO_REPO_PATH || ''
   const branch = "main"
   const message = `autocommit from bearblog.app at ${gitTimestamp()}`
 
-  //await gitAdd(directory);
-
-  //await gitCommit(message, directory);
-
-  //await gitPush(branch, directory);
+  await gitAdd(directory);
+  await gitCommit(message, directory);
+  await gitPush(branch, directory);
 
 }
 
