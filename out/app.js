c72065ad6d8b1ce1b3fbdc3b681c0acc0acf4f3d
Tim Duckett <tim@duckett.de>
2023-10-28 17:07:04 +0200

    refactoring db

--- out/app.js
+++ out/app.js
@@ -1,102 +1,28 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const dbHelpers_1 = require("./dbHelpers");
+const dbHelpers_2 = require("./dbHelpers");
 console.log("Running...");
 const bearDb = (0, dbHelpers_1.connectToBearDb)();
 const localDb = (0, dbHelpers_1.connectToLocalDb)();
-// const bearBlogDbPath = path.join(__dirname, '..', 'db', 'bearblog.db');
-// console.log(bearBlogDbPath)
-// const localDb = new sqlite3.Database(bearBlogDbPath, (err) => {
-//   if (err) {
-//     return console.log('Error:', err.message);
-//   }
-//   console.log('Connected to the bearblog database.');
-// });
-// Access the database
-bearDb.serialize(() => {
-    // Step 1: Retrieve the Z_PK value from the ZSFNOTETAG table where the ZTITLE field = "blog-from-bear"
-    bearDb.get(`SELECT Z_PK FROM ZSFNOTETAG WHERE ZTITLE = ?`, ['blog-from-bear'], (err, row) => {
-        var _a;
-        if (err) {
-            console.log('Error:', err.message);
-            return;
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const taggedNotes = yield (0, dbHelpers_2.fetchBearData)(bearDb);
+        if (taggedNotes) {
+            console.log(taggedNotes.length);
+            (0, dbHelpers_2.saveToLocalDatabase)(localDb, taggedNotes);
         }
-        const zPkValue = (_a = row === null || row === void 0 ? void 0 : row.Z_PK) !== null && _a !== void 0 ? _a : null;
-        // Step 2: Retrieve all Z_13TAGS values from the Z_5TAGS table where the Z_13TAGS value is the Z_PK value retrieved from the previous step
-        bearDb.all(`SELECT Z_5NOTES FROM Z_5TAGS WHERE Z_13TAGS = ?`, [zPkValue], (err, rows) => {
-            if (err) {
-                console.log('Error:', err.message);
-                return;
-            }
-            const z13TagsValues = rows.map(r => r.Z_5NOTES);
-            // Step 3: Retrieve all fields for all records from the ZSFNOTE table where the Z_PK values are included in the set returned in the previous step
-            const placeHolders = z13TagsValues.map(() => '?').join(',');
-            const sql = `
-        SELECT 
-          Z_PK, ZTITLE, ZTEXT, ZUNIQUEIDENTIFIER, ZMODIFICATIONDATE, ZARCHIVED 
-        FROM 
-          ZSFNOTE 
-        WHERE 
-          Z_PK IN (${placeHolders})
-          AND 
-          ZARCHIVED = 0
-        `;
-            bearDb.all(sql, z13TagsValues, (err, rows) => {
-                if (err) {
-                    console.log('Error:', err.message);
-                    return;
-                }
-                // Print the retrieved records
-                console.log('Retrieved Records:', rows);
-                saveToBearBlogDatabase(rows);
-                bearDb.close((err) => {
-                    if (err) {
-                        console.log('Error:', err.message);
-                    }
-                    console.log('Database connection closed.');
-                });
-            });
-        });
     });
+}
+main().catch((error) => {
+    console.error(`An error occurred: ${error}`);
 });
-const saveToBearBlogDatabase = (rows) => {
-    rows.forEach((row) => {
-        // Check if a record with the same Z_PK exists
-        localDb.get('SELECT * FROM ZSFNOTE WHERE Z_PK = ?', [row.Z_PK], (err, existingRow) => {
-            if (err) {
-                return console.log('Select Error:', err.message);
-            }
-            // If a matching record is found
-            if (existingRow) {
-                // Compare ZMODIFICATIONDATE
-                if (new Date(row.ZMODIFICATIONDATE) > new Date(existingRow.ZMODIFICATIONDATE)) {
-                    // Update the record if the new ZMODIFICATIONDATE is greater
-                    const updateQuery = `
-            UPDATE ZSFNOTE
-            SET ZTITLE = ?, ZTEXT = ?, ZUNIQUEIDENTIFIER = ?, ZMODIFICATIONDATE = ?, ZARCHIVED = ?, UPDATED = 1
-            WHERE Z_PK = ?;
-          `;
-                    localDb.run(updateQuery, [row.ZTITLE, row.ZTEXT, row.ZUNIQUEIDENTIFIER, row.ZMODIFICATIONDATE, row.ZARCHIVED, row.Z_PK], (err) => {
-                        if (err) {
-                            return console.log('Update Error:', err.message);
-                        }
-                        console.log(`Row updated with ID: ${row.Z_PK}`);
-                    });
-                }
-            }
-            else {
-                // If no matching record is found, insert a new record
-                const insertQuery = `
-          INSERT INTO ZSFNOTE (Z_PK, ZTITLE, ZTEXT, ZUNIQUEIDENTIFIER, ZMODIFICATIONDATE, ZARCHIVED)
-          VALUES (?, ?, ?, ?, ?, ?);
-        `;
-                localDb.run(insertQuery, [row.Z_PK, row.ZTITLE, row.ZTEXT, row.ZUNIQUEIDENTIFIER, row.ZMODIFICATIONDATE, row.ZARCHIVED], (err) => {
-                    if (err) {
-                        return console.log('Insert Error:', err.message);
-                    }
-                    console.log(`Row inserted with ID: ${row.Z_PK}`);
-                });
-            }
-        });
-    });
-};
--- src/dbHelpers.ts
+++ src/dbHelpers.ts
@@ -1,11 +1,16 @@
 import os from 'os';
 import path from 'path';
+import dotenv from 'dotenv';
 import sqlite3, { Database } from 'sqlite3';
+import { ZSFNoteRow, ZSFNoteTagRow, Z_5TAGSRow } from './interfaces';
 
+dotenv.config();
 
 export function connectToBearDb(): Database {
 
-    const dbPath = path.join(os.homedir(), 'Library/Group Containers/9K33E3U3T4.net.shinyfrog.bear/Application Data/database.sqlite');
+    const BEAR_DB_PATH = process.env.BEAR_DB_PATH || ""
+
+    const dbPath = path.join(os.homedir(), BEAR_DB_PATH);
 
     const db = new sqlite3.Database(dbPath, (err) => {
         if (err) {
@@ -20,7 +25,10 @@ export function connectToBearDb(): Database {
 
 export function connectToLocalDb(): Database {
 
-    const localDbPath = path.join(__dirname, '..', 'db', 'bearblog.db');
+    const LOCAL_DB_DIR = process.env.LOCAL_DB_DIR || ''
+    const LOCAL_DB = process.env.LOCAL_DB || ''
+
+    const localDbPath = path.join(__dirname, '..', LOCAL_DB_DIR, LOCAL_DB);
     
     const localDb = new sqlite3.Database(localDbPath, (err) => {
         if (err) {
@@ -31,3 +39,118 @@ export function connectToLocalDb(): Database {
 
     return localDb;
 }
+
+export function saveToLocalDatabase(localDb: Database, rows: ZSFNoteRow[]) {
+    rows.forEach((row) => {
+      // Check if a record with the same Z_PK exists
+      localDb.get('SELECT * FROM ZSFNOTE WHERE Z_PK = ?', [row.Z_PK], (err, existingRow: ZSFNoteRow) => {
+        if (err) {
+          return console.log('Select Error:', err.message);
+        }
+  
+        // If a matching record is found
+        if (existingRow) {
+          // Compare ZMODIFICATIONDATE
+          if (new Date(row.ZMODIFICATIONDATE) > new Date(existingRow.ZMODIFICATIONDATE)) {
+            // Update the record if the new ZMODIFICATIONDATE is greater
+            const updateQuery = `
+              UPDATE ZSFNOTE
+              SET ZTITLE = ?, ZTEXT = ?, ZUNIQUEIDENTIFIER = ?, ZMODIFICATIONDATE = ?, ZARCHIVED = ?, UPDATED = 1
+              WHERE Z_PK = ?;
+            `;
+            localDb.run(updateQuery, [row.ZTITLE, row.ZTEXT, row.ZUNIQUEIDENTIFIER, row.ZMODIFICATIONDATE, row.ZARCHIVED, row.Z_PK], (err) => {
+              if (err) {
+                return console.log('Update Error:', err.message);
+              }
+              console.log(`Row updated with ID: ${row.Z_PK}`);
+            });
+          }
+        } else {
+          // If no matching record is found, insert a new record
+          const insertQuery = `
+            INSERT INTO ZSFNOTE (Z_PK, ZTITLE, ZTEXT, ZUNIQUEIDENTIFIER, ZMODIFICATIONDATE, ZARCHIVED)
+            VALUES (?, ?, ?, ?, ?, ?);
+          `;
+          localDb.run(insertQuery, [row.Z_PK, row.ZTITLE, row.ZTEXT, row.ZUNIQUEIDENTIFIER, row.ZMODIFICATIONDATE, row.ZARCHIVED], (err) => {
+            if (err) {
+              return console.log('Insert Error:', err.message);
+            }
+            console.log(`Row inserted with ID: ${row.Z_PK}`);
+          });
+        }
+      });
+    });
+}
+
+const getBloggingTagId = async (bearDb: Database): Promise<number | null> => {
+    return new Promise((resolve, reject) => {
+        bearDb.get(`SELECT Z_PK FROM ZSFNOTETAG WHERE ZTITLE = ?`, ['blog-from-bear'], (err, row) => {
+            if (err) {
+                console.log('Error:', err.message);
+                reject(err);
+            } else {
+                const zPkValue = (row as ZSFNoteTagRow)?.Z_PK ?? null;
+                resolve(zPkValue);
+            }
+        });
+    });
+};
+
+const getTaggedNoteIds = async (bearDb: Database, zPkValue: number | null): Promise<number[]> => {
+    return new Promise((resolve, reject) => {
+        bearDb.all(`SELECT Z_5NOTES FROM Z_5TAGS WHERE Z_13TAGS = ?`, [zPkValue], (err, rows) => {
+            if (err) {
+                console.log('Error:', err.message);
+                reject(err);
+            } else {
+                const z13TagsValues = (rows as Z_5TAGSRow[]).map(r => r.Z_5NOTES);
+                resolve(z13TagsValues);
+            }
+        });
+    });
+};
+
+const fetchTaggedNotes = async (bearDb: Database, z13TagsValues: number[]): Promise<ZSFNoteRow[]> => {
+    return new Promise((resolve, reject) => {
+        const placeHolders = z13TagsValues.map(() => '?').join(',');
+        const sql = `
+        SELECT 
+            Z_PK, ZTITLE, ZTEXT, ZUNIQUEIDENTIFIER, ZMODIFICATIONDATE, ZARCHIVED 
+        FROM 
+            ZSFNOTE 
+        WHERE 
+            Z_PK IN (${placeHolders})
+            AND 
+            ZARCHIVED = 0
+        `;
+
+        bearDb.all(sql, z13TagsValues, (err, rows) => {
+            if (err) {
+                console.log('Error:', err.message);
+                reject(err);
+            } else {
+                resolve(rows as ZSFNoteRow[]);
+            }
+        });
+    });
+};
+
+// Main function that executes all steps
+export async function fetchBearData(bearDb: Database) {
+    try {
+        const bloggingTagId = await getBloggingTagId(bearDb);
+        const taggedNoteIds = await getTaggedNoteIds(bearDb, bloggingTagId);
+        const taggedNotes = await fetchTaggedNotes(bearDb, taggedNoteIds);
+        return taggedNotes
+    } catch (err) {
+        console.log('An error occurred:', err);
+    } finally {
+        bearDb.close(err => {
+            if (err) {
+                console.log('Error:', err.message);
+            }
+            console.log('Database connection closed.');
+        });
+    }
+}
+
